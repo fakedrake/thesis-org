3f141c5389c6df637ac79349d41dbc94815ccd0d
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-12-03 20:40:54 +0000

    Sync

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((compile-root . "/Users/cperivol/Projects/phd/thesis-org/")
-         (compile-command . "rm -rf build/ && ./build.sh"))))
+         (compile-command . "rm -rf build/ && ./build.sh")
+         (reftex-default-bibliography "./bibliography.bib"))))
