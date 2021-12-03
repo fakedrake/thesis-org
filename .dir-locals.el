11c031af883743c3178420a016fd5ea73618b08d
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-12-03 21:04:12 +0000

    Fixed bibliography

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((compile-root . "/Users/cperivol/Projects/phd/thesis-org/")
-         (compile-command . "rm -rf build/ && ./build.sh")
-         (reftex-default-bibliography "./bibliography.bib"))))
+         (compile-command . "./build.sh")
+         (reftex-default-bibliography . ("./bibliography.bib")))))
