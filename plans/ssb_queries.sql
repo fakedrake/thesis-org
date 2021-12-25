c4931fb96392d07e7473a09d50ea49de4ff01b7b
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-12-25 01:19:33 +0000

    Sync

--- plans/ssb_queries.sql
+++ plans/ssb_queries.sql
@@ -5,7 +5,7 @@ from lineorder, date
 where lo_orderdate = d_datekey
 and d_yearmonthnum = 199401
 and lo_discount between 4 and 6
-and lo_quantity between 26 and 35"
+and lo_quantity between 26 and 35
 
 -- Query 2
 select sum(lo_extendedprice*lo_discount) as revenue
@@ -13,7 +13,7 @@ from lineorder, date
 where lo_orderdate = d_datekey
 and d_year = 1993
 and lo_discount between 1 and 3
-and lo_quantity < 25"
+and lo_quantity < 25
 
 -- Query 3
 select sum(lo_extendedprice*lo_discount) as revenue
@@ -21,7 +21,7 @@ from lineorder, date
 where lo_orderdate = d_datekey
 and d_yearmonthnum = 199401
 and lo_discount between 4 and 6
-and lo_quantity between 26 and 35"]
+and lo_quantity between 26 and 35
 
 -- Flight 2
 -- Query 4
@@ -33,7 +33,7 @@ and lo_suppkey = s_suppkey
 and p_category = 'MFGR#12'
 and s_region = 'AMERICA'
 group by d_year, p_brand1
-order by d_year, p_brand1"
+order by d_year, p_brand1
 
 -- Query 5
 select sum(lo_revenue), d_year, p_brand1
@@ -44,7 +44,7 @@ and lo_suppkey = s_suppkey
 and p_brand1 between 'MFGR#2221' and 'MFGR#2228'
 and s_region = 'ASIA'
 group by d_year, p_brand1
-order by d_year, p_brand1"
+order by d_year, p_brand1
 
 -- Query 6
 select sum(lo_revenue), d_year, p_brand1
@@ -55,7 +55,7 @@ and lo_suppkey = s_suppkey
 and p_brand1 = 'MFGR#2221'
 and s_region = 'EUROPE'
 group by d_year, p_brand1
-order by d_year, p_brand1"
+order by d_year, p_brand1
 
 -- Flight 3
 -- Query 7
@@ -67,7 +67,7 @@ and lo_orderdate = d_datekey
 and c_region = 'ASIA' and s_region = 'ASIA'
 and d_year >= 1992 and d_year <= 1997
 group by c_nation, s_nation, d_year
-order by d_year, revenue desc"
+order by d_year, revenue desc
 
 -- Query 8
 select c_city, s_city, d_year, sum(lo_revenue) as revenue
@@ -79,7 +79,7 @@ and c_nation = 'UNITED STATES'
 and s_nation = 'UNITED STATES'
 and d_year >= 1992 and d_year <= 1997
 group by c_city, s_city, d_year
-order by d_year, revenue desc"
+order by d_year, revenue desc
 
 -- Query 9
 select c_city, s_city, d_year, sum(lo_revenue) as revenue
@@ -91,7 +91,7 @@ and (c_city='UNITED KI1' or c_city='UNITED KI5')
 and (s_city='UNITED KI1' or s_city='UNITED KI5')
 and d_year >= 1992 and d_year <= 1997
 group by c_city, s_city, d_year
-order by d_year, revenue desc"
+order by d_year, revenue desc
 
 -- Query 10
 select c_city, s_city, d_year, sum(lo_revenue) as revenue
@@ -103,7 +103,7 @@ and (c_city='UNITED KI1' or c_city='UNITED KI5')
 and (s_city='UNITED KI1' or s_city='UNITED KI5')
 and d_yearmonth = 'Dec1997'
 group by c_city, s_city, d_year
-order by d_year, revenue desc"
+order by d_year, revenue desc
 
 -- Flight 4
 -- Query 11
@@ -117,7 +117,7 @@ where lo_custkey = c_custkey
  and s_region = 'AMERICA'
  and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
 group by d_year, c_nation
-order by d_year, c_nation"
+order by d_year, c_nation
 
 -- Query 12
 select d_year, s_nation, p_category, sum(lo_revenue - lo_supplycost) as profit
@@ -131,7 +131,7 @@ and s_region = 'AMERICA'
 and (d_year = 1997 or d_year = 1998)
 and (p_mfgr = 'MFGR#1'
 or p_mfgr = 'MFGR#2')
-group by d_year, s_nation, p_category order by d_year, s_nation, p_category"
+group by d_year, s_nation, p_category order by d_year, s_nation, p_category
 
 -- Query 13
 select d_year, s_city, p_brand1, sum(lo_revenue - lo_supplycost) as profit
