495079b19c5e7df48848735628d81b8d8847dc4d
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2022-10-27 19:38:35 +0100

    Pdf

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((compile-root . "/Users/cperivol/Projects/phd/thesis-org/")
-         (compile-command . "aspell --help")
+         (compile-command . "./build.sh")
          (reftex-refstyle . "\\ref")
          (TeX-master . "./thesis.tex")
          (reftex-default-bibliography . ("./bibliography.bib")))))
